cf950b016df8665ad7d3edf525dcd430d9828251
xjd <lynndon@gmail.com>
2024-08-29 09:26:29 +0800

    Add cargo publish CI

--- serde_molecule/src/lib.rs
+++ serde_molecule/src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![doc = include_str!("../../README.md")]
+#![doc = include_str!("../README.md")]
 
 #[cfg(not(any(feature = "std", feature = "alloc")))]
 compile_error! {
